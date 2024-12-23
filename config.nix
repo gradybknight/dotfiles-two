c16cf3f10862f9bb922bcbc7d8d50f6d19132075
gradybknight <grady.knight@gmail.com>
2024-12-23 11:15:02 -0500

    add devpod setup

---
+++ config.nix
@@ -0,0 +1,18 @@
+{
+  packageOverrides = pkgs: with pkgs; {
+    myPackages = pkgs.buildEnv {
+      name = "grady-tools";
+      paths = [
+        neovim
+        ripgrep
+        tmux
+        tree
+        pure-prompt
+        fzf
+        jq
+        nodejs_22
+      ];
+    };
+  };
+}
+
